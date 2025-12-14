7ac1b767e56f46aa9c267a6166778df4210f5cc6
reham <najehreham9@gmail.com>
2025-12-14 17:08:34 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookup/features/splash/presentation/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
