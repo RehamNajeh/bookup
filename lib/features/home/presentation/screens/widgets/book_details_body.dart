758d38a3d27bb69ddfaa86a7830bdb5bfb701e99
reham <najehreham9@gmail.com>
2025-12-19 20:15:02 +0200

    feat: enhance BookDetailsBody with title, author, and rating display

--- lib/features/home/presentation/screens/widgets/book_details_body.dart
+++ lib/features/home/presentation/screens/widgets/book_details_body.dart
@@ -1,3 +1,6 @@
+import 'package:bookup/core/constants/app_constants.dart';
+import 'package:bookup/core/utils/styles.dart';
+import 'package:bookup/features/home/presentation/screens/widgets/book_rating_widget.dart';
 import 'package:bookup/features/home/presentation/screens/widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
@@ -7,14 +10,29 @@ class BookDetailsBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      crossAxisAlignment: .center,
-      children: [
-        const BookDetailsAppBar(),
-        Padding(padding: EdgeInsets.symmetric(horizontal: width * 0.15), 
-        child: const CustomListViewItem()
-        ),
-      ],
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: .center,
+        children: [
+          const BookDetailsAppBar(),
+          Padding(padding: EdgeInsets.symmetric(horizontal: width * 0.15), 
+          child: const CustomListViewItem()
+      
+          ),
+          SizedBox(height: 44),
+          Text("The Jungle Book", style: Styles.textStyle30.copyWith(
+            fontFamily: AppConstants.kGtSectraFine,
+          ),),
+          SizedBox(height: 3),
+          Text("Rudyard Kipling", style: Styles.textStyle18.copyWith(
+            color: Colors.grey[600],
+            fontStyle: FontStyle.italic,
+          ),
+          ),
+          const SizedBox(height: 18),
+           BookRating( mainAxisAlignment: MainAxisAlignment.center,),
+        ],
+      ),
     );
   }
 }
