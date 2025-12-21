937f107323c786883dc8698fd803d5acaac52d88
reham <najehreham9@gmail.com>
2025-12-21 20:53:15 +0200

    feat: implement fetchFeaturedBooks method and add FeaturedBooksCubit state management

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -29,7 +29,21 @@ class HomeRepoImpl implements HomeRepo {
   }
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+     try {
+      var data = await apiService.get(
+        endpoint: 'volumes?Filtering=free-ebooks&q=Programming',
+      );
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return Left(ServerFailure.fromDioError(e));
+      }
+      return Left(ServerFailure(e.toString()));
+    }
   }
 }
---
+++ lib/features/home/presentation/manager/features_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bookup/features/home/data/models/book_model.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
+
