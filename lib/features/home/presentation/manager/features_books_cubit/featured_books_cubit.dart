13703a71e900c22c70ad52c8dd8ec2b4beef9fa5
reham <najehreham9@gmail.com>
2025-12-21 21:09:15 +0200

    feat: create fetchFeaturedBooks method

--- lib/features/home/presentation/manager/features_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/features_books_cubit/featured_books_cubit.dart
@@ -1,10 +1,24 @@
+import 'dart:math';
+
+import 'package:bookup/core/errors/failure.dart';
 import 'package:bookup/features/home/data/models/book_model.dart';
+import 'package:bookup/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+   emit(FeaturedBooksLoading());
+   var result = await homeRepo.fetchFeaturedBooks();
+   result.fold((failure){
+    emit(FeaturedBooksFailure(failure.errorMessage));
+   }, (featuredBooks){
+    emit(FeaturedBooksSuccess(featuredBooks));
+   });
+  }
 }
 
